69932551292199cd14ba441f5395d19a1eeb7a0a
kevin <kevinallen122451@gmail.com>
2026-08-28 14:25:12 +0530

    Docker Updated

--- Backend/app/services/firestore.py
+++ Backend/app/services/firestore.py
@@ -1,11 +1,3 @@
-from google.cloud import firestore
-from google.oauth2 import service_account
+from firebase_admin import firestore
 
-# Path to your Firebase service account key
-SERVICE_ACCOUNT_FILE = "firebase-service-account.json"
-
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE
-)
-
-db = firestore.Client(credentials=credentials, project=credentials.project_id)
+db = firestore.client()
